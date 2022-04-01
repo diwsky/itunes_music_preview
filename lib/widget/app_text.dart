44754e80bd6390110097e95685a57c24ea7c558a
Rizky Agung Ramadhan <rizkyagungramadhan@gmail.com>
2022-04-01 19:03:47 +0700

    Create app_text.dart

---
+++ lib/widget/app_text.dart
@@ -0,0 +1,23 @@
+import 'package:flutter/material.dart';
+import 'package:itunes_music_preview/style/app_text_style.dart';
+
+/// Created by rizkyagungramadhan@gmail.com
+/// on 4/1/2022.
+
+
+class AppText extends StatelessWidget {
+  final String text;
+  final int maxLines;
+  final TextStyle? style;
+  const AppText(this.text, {this.maxLines = 1, this.style, Key? key}) : super(key: key);
+
+  @override
+  Widget build(BuildContext context) {
+    return Text(
+      text,
+      maxLines: maxLines,
+      style: style ?? AppTextStyle.light(),
+      overflow: TextOverflow.ellipsis,
+    );
+  }
+}
