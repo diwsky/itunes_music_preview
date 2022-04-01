0faec998694ec2bf0f44756dc11dc1e51705fb6b
Rizky Agung Ramadhan <rizkyagungramadhan@gmail.com>
2022-04-01 13:03:42 +0700

    create app pages route

---
+++ lib/routes/app_page.dart
@@ -0,0 +1,14 @@
+import 'package:get/get.dart';
+import 'package:itunes_music_preview/module/main/main_page.dart';
+import 'package:itunes_music_preview/routes/app_route.dart';
+
+/// Created by rizkyagungramadhan@gmail.com
+/// on 4/1/2022.
+
+class AppPage {
+  const AppPage._();
+
+  static final routes = [
+    GetPage(name: AppRoute.main, page: () => const MainPage())
+  ];
+}
