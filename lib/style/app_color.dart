4d9f5bbce1e26ec9effe2a9934bc9b1a19ae0234
Rizky Agung Ramadhan <rizkyagungramadhan@gmail.com>
2022-04-01 13:47:04 +0700

    create app common style classes

---
+++ lib/style/app_color.dart
@@ -0,0 +1,12 @@
+import 'package:flutter/material.dart';
+
+/// Created by rizkyagungramadhan@gmail.com
+/// on 4/1/2022.
+
+
+class AppColor {
+  const AppColor._();
+
+  static const primary = Color(0xFF1AA7EC);
+  static const accent = Color(0xFF414C6B);
+}
---
+++ lib/style/app_theme.dart
@@ -0,0 +1,17 @@
+import 'package:flutter/cupertino.dart';
+import 'package:flutter/material.dart';
+import 'package:itunes_music_preview/style/app_color.dart';
+import 'package:itunes_music_preview/style/app_text_style.dart';
+
+/// Created by rizkyagungramadhan@gmail.com
+/// on 4/1/2022.
+
+class AppTheme {
+  const AppTheme._();
+
+  static final ThemeData theme = ThemeData(
+    primaryColor: AppColor.primary,
+    backgroundColor: CupertinoColors.systemGrey6,
+    colorScheme: ColorScheme.fromSwatch().copyWith(secondary: AppColor.accent),
+  );
+}
